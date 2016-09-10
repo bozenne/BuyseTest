68ef7f59ccbae00da257937b15a3b24e40f95ad0
bozenne <broz@sund.ku.dk>
2016-09-10 21:06:36 +0200

    add loading file

--- R/onload.R
+++ R/onload.R
@@ -1,4 +1,5 @@
 '.onAttach' <- function(libname, pkgname="BuyseTest") {
   desc <- utils::packageDescription(pkgname)
   packageStartupMessage(desc$Package, " version ",desc$Version)
+  BuyseTest.options(reinitialise = TRUE) # generate .BuyseTest-options when loading the package
 }
